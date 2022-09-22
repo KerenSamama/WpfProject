eb9a3cb84c1430ed6967dc6d7934760bccade3d0
Léa Scetbon <scetbon@g.jct.ac.il>
2022-09-22 14:23:48 +0300

    weatheradapter

--- DAL/WeatherAdapter.cs
+++ DAL/WeatherAdapter.cs
@@ -8,6 +8,10 @@ namespace DAL
 {
     public class WeatherAdapter
     {
+        private const string StartURLWeather = @"http://api.openweathermap.org/data/2.5/weather?q=";
+        private const string EndURLWeather = @"&APPID=e4aad74d6a7067935506d3281bbc41e8";
+
+
 
     }
 }
