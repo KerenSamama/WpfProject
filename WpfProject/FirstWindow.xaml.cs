268c21a8a4f67f4029b4049eebe24f49edf92b02
Keren Samama <ksamama@g.jct.ac.il>
2022-11-09 12:19:41 +0200

    FirstWindows

---
+++ WpfProject/FirstWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+
+namespace WpfProject
+{
+    /// <summary>
+    /// Logique d'interaction pour FirstWindow.xaml
+    /// </summary>
+    public partial class FirstWindow : Window
+    {
+        public FirstWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
